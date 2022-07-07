175b9811bb74e7a2ad7cc67bff3460b09fa2d6ec
Augusto Balansó <38744332+augustobalanso@users.noreply.github.com>
2022-07-07 20:06:00 -0300

    2do desafio fix

--- index.js
+++ index.js
@@ -1,91 +1,84 @@
 const fs = require('fs');
 
 class Contenedor {
-    constructor (title, price, thumbnail) {
-        this.title = title
-        this.price = price
-        this.thumbnail = thumbnail
+    constructor (fileName) {
+        this.fileName = fileName
     }
 
-    async save(object) { 
+
+    async save(array) { 
         
         try {
-            const productsOnFile = fs.promises.readFile('./productos.txt')
-                .then((res) => {                
-                    const parsedRes = JSON.parse(res);
-                    if(parsedRes.length == 0){
-                        object.id = 1
-                    } else {
-                        object.id = parsedRes[parsedRes.length - 1].id + 1
-                    }
+            const productsFetch = JSON.parse(await fs.promises.readFile(this.fileName,"utf-8"))
+            let id = 1
 
-                    return parsedRes
-                }
-                )
-            console.log("Archivo:", await productsOnFile);
-
-            const newArrayToUpload = await productsOnFile
-            newArrayToUpload.push(object)
-            console.log(newArrayToUpload)
-            fs.writeFileSync('./productos.txt', JSON.stringify(newArrayToUpload))
+            if(productsFetch.length === 0){
+                array.forEach(element => { 
+                    element.id = id
+                    id++
+                });
+            } else {
+                array.forEach(element => {
+                    element.id = productsFetch[productsFetch.length-1].id + id
+                    id++
+                })
             }
 
-            catch(err){
-                console.log('error: ' , err)
-            }
+            array.forEach((el) => {
+                productsFetch.push(el)
+            })
+
+            await fs.promises.writeFile('./productos.txt', JSON.stringify(productsFetch))
+            console.log('Agregando los sig. objetos al archivo...')
+            console.log(array)
+            console.log('Archivo guardado!')
+        }
+
+        catch(err){
+            console.log('error: ' , err)
+        }
     }
 
-    getById(idFilter){
-        // const searchableArray = JSON.parse(fs.readFileSync('./productos.txt'))
+    async getById(idFilter){
+        const searchableArray = JSON.parse(await fs.promises.readFile(this.fileName))
 
-        // // CHECKS IF ID EXISTS, ERROR IF NOT
-        // if(searchableArray.some(element => element.id === idFilter)){
-        //     const filteredObject = searchableArray.filter(element => element.id == idFilter)
-        //     console.log(filteredObject)
-        // } else {
-        //     console.log(`Error de filtrado: id ${idFilter} no encontrado`)
-        // }
+        // CHECKS IF ID EXISTS, ERROR IF NOT
+        if(searchableArray.some(element => element.id === idFilter)){
+            const filteredObject = searchableArray.filter(element => element.id == idFilter)
+            console.log(filteredObject)
+        } else {
+            console.log(`Error de filtrado: id ${idFilter} no encontrado`)
+        }
     }
 
-    getAll(){
-        // const readArray = JSON.parse(fs.readFileSync('./productos.txt'))
-        // return console.log(readArray)
+    async getAll(){
+        const readArray = JSON.parse(await fs.promises.readFile(this.fileName))
+        return console.log('respuesta de getAll(): ', readArray)
     }
 
-    deleteById(idDelete){
+    async deleteById(idDelete){
 
-        // function existsDelete() {
-        //     const filteredObject = searchableArray.filter(element => element.id != idDelete)
-        //     console.log(filteredObject)
-        //     fs.writeFileSync('./productos.txt',JSON.stringify(filteredObject))
-        // }
-
-        // function notFoundDelete() {
-        //     return (console.log(`Error de borrado: id ${idDelete} no encontrado`))
-        // }
-
-        // const searchableArray = JSON.parse(fs.readFileSync('./productos.txt'))
-        // if(searchableArray.some(element => element.id === idDelete)){
-        //     existsDelete() 
-        // } else {
-        //     notFoundDelete()
-        // }
-
-        // console.log(JSON.parse(fs.readFileSync('./productos.txt')))
+        const searchableArray = JSON.parse(await fs.promises.readFile(this.fileName))
+        if(searchableArray.some(element => element.id === idDelete)){
+            const filteredObject = searchableArray.filter(element => element.id != idDelete)
+            console.log('Borrando ID...')
+            await fs.promises.writeFile(this.fileName,JSON.stringify(filteredObject)) 
+        } else {
+            return (console.log(`Error de borrado: id ${idDelete} no encontrado`))
+        }
 
     }
 
-    deleteAll(){
-        // fs.writeFileSync('./productos.txt',JSON.stringify([]))
+    async deleteAll(){
+        await fs.promises.writeFile(this.fileName,JSON.stringify([]))
+        console.log('Base de datos eliminada')
     }
 }
 
-const initPrueba = new Contenedor()
+const initPrueba = new Contenedor('./productos.txt')
 
-initPrueba.save({"title":"producto1","price":2000,"thumbnail":"./img/01.jpg"})
-initPrueba.save({"title":"producto2","price":3000,"thumbnail":"./img/02.jpg"})
-initPrueba.save({"title":"producto3","price":4500,"thumbnail":"./img/03.jpg"})
-// initPrueba.getById('1')
+initPrueba.save([{"title":"producto1","price":2000,"thumbnail":"./img/01.jpg"},{"title":"producto2","price":3000,"thumbnail":"./img/02.jpg"},{"title":"producto3","price":4500,"thumbnail":"./img/03.jpg"}])
+// initPrueba.getById(1)
 // initPrueba.getAll()
-// initPrueba.deleteById('2')
+// initPrueba.deleteById(3)
 // initPrueba.deleteAll()
